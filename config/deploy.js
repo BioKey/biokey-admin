a4745dd35b2238ed2e11fe4071370ce21398dc2c
Tony Wu <tony_yz_wu@hotmail.com>
2018-03-21 23:54:30 -0400

    More deployment code.

--- config/deploy.js
+++ config/deploy.js
@@ -13,7 +13,8 @@ module.exports = function(deployTarget) {
     	accessKeyId: process.env.AWS_ACCESS_KEY_ID,
     	secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
     	bucket: process.env.AWS_DEPLOY_S3_BUCKET,
-    	region: process.env.AWS_REGION
+    	region: process.env.AWS_REGION,
+    	filePattern: '**/*.{js,css,png,gif,ico,jpg,map,xml,txt,svg,swf,eot,ttf,woff,woff2,html}'
     }
   };
 
