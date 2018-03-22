fdfcd06a0cf18512053e9a8378254ab8d6fdd0cb
Tony Wu <tony_yz_wu@hotmail.com>
2018-03-21 22:01:13 -0400

    Get ready for production.

--- config/deploy.js
+++ config/deploy.js
@@ -1,11 +1,29 @@
-module.exports = function(deployTarget) {
+/* eslint-env node */
+'use strict';
 
-  var ENV = {
-    build: {
-      environment: deployTarget
-    }
+module.exports = function(deployTarget) {
+  let ENV = {
+    build: {}
+    // include other plugin configuration that applies to all deploy targets here
   };
 
-  return ENV;
+  if (deployTarget === 'development') {
+    ENV.build.environment = 'development';
+    // configure other plugins for development deploy target here
+  }
 
+  if (deployTarget === 'staging') {
+    ENV.build.environment = 'production';
+    // configure other plugins for staging deploy target here
+  }
+
+  if (deployTarget === 'production') {
+    ENV.build.environment = 'production';
+    // configure other plugins for production deploy target here
+  }
+
+  // Note: if you need to build some configuration asynchronously, you can return
+  // a promise that resolves with the ENV object instead of returning the
+  // ENV object synchronously.
+  return ENV;
 };
